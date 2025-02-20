d6446d673bbe62c252898c0b7eaacbb064debba9
Lito Croy <146680987+litobot@users.noreply.github.com>
2025-02-20 14:37:34 -0700

    Initial commit

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  const dummyIdeas = [
+    { id: 1, title: 'Prank Travis', description: 'Stick googly eyets on all his stuff' },
+    { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both neither one of you is lying' },
+    { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+  ]
+  const [ideas, setIdeas] = useState(dummyIdeas);
+
+  return (
+    <main classname='App'>
+      <h1>IdeaBox</h1>
+      <p>Hi!</p>
+    </main>
+  )
+}
+
+export default App;
